3c116c9c828daaefc564edb8d138684a8dd2b625
xiaonanpols21 <xiaonanpolspols@gmail.com>
2023-01-19 16:26:43 +0100

    vincent helpt

--- scripts/d3-3.js
+++ scripts/d3-3.js
@@ -353,21 +353,28 @@ var xCenter = [-50, 300, 500, 800];
 var yCenter = [200, -50, 200, 100];
 
 function updateKoppigheid(data, buttonPressed, whichBtn ) {
+
     // Koppigheid
-    let newData;
+    let newData = [];
+    // Zoek alle knoppen met de class 'active'. Voor elke knop, kijken we naar het label en gebruiken we dat als filter.
+    // Doordat we .push gebruiken breiden we de newData array telkens uit met meer data als er meer knoppen actief zijn
+
     if (buttonPressed) {
-        if (whichBtn == 1) {
-            newData = data.filter(d => d.koppigheid == "1-15");
-        } else if (whichBtn == 2) {
-            newData = data.filter(d => d.koppigheid == "16-31");
-        } else if (whichBtn == 3) {
-            newData = data.filter(d => d.koppigheid == "32-45");
-        } else if (whichBtn == 4) {
-            newData = data.filter(d => d.koppigheid == "46-50");
-        }
+        document.querySelectorAll("#koppigheid button.active").forEach(button => {
+            const dataFilter = data.filter(d => d.koppigheid == button.innerText);
+            console.log(dataFilter);
+            newData.push(dataFilter);
+        })
     } else {
         newData = data;
     };
+
+    // Zijn er geen knoppen met de class active? Dan blijft newData leeg en vullen we het met data:
+    if (newData.length == 0) {
+        newData = data; 
+    }
+    
+    console.log(newData);
 
     d3.forceSimulation(data)
         .force('charge', d3.forceManyBody().strength(20))
@@ -401,119 +408,120 @@ function updateKoppigheid(data, buttonPressed, whichBtn ) {
     };
 };
 
-function updateSchermtijd(data, buttonPressed, whichBtn ) {
-    // Koppigheid
-    let newData;
-    if (buttonPressed) {
-        if (whichBtn == 1) {
-            newData = data.filter(d => d.schermtijd == "1-3");
-        } else if (whichBtn == 2) {
-            newData = data.filter(d => d.schermtijd == "4-7");
-        } else if (whichBtn == 3) {
-            newData = data.filter(d => d.schermtijd == "8+");
-        } 
-    } else {
-        newData = data;
-    };
+// function updateSchermtijd(data, buttonPressed, whichBtn ) {
+//     // Koppigheid
+//     let newData;
+//     if (buttonPressed) {
+//         if (whichBtn == 1) {
+//             newData = data.filter(d => d.schermtijd == "1-3");
+//         } else if (whichBtn == 2) {
+//             newData = data.filter(d => d.schermtijd == "4-7");
+//         } else if (whichBtn == 3) {
+//             newData = data.filter(d => d.schermtijd == "8+");
+//         } 
+//     } else {
+//         newData = data;
+//     };
 
-    d3.forceSimulation(data)
-        .force('charge', d3.forceManyBody().strength(20))
-        .force('x', d3.forceX().x(function(d) {
-            return xCenter[d.gekozenTiktok];
-        }))
-        .force('y', d3.forceY().y(function(d) {
-            return yCenter[d.gekozenTiktok];
-        }))
-        .force('collision', d3.forceCollide().radius(function(d) {
-            return 25;
-        }))
-        .on('tick', ticked)
-    ;
+//     d3.forceSimulation(data)
+//         .force('charge', d3.forceManyBody().strength(20))
+//         .force('x', d3.forceX().x(function(d) {
+//             return xCenter[d.gekozenTiktok];
+//         }))
+//         .force('y', d3.forceY().y(function(d) {
+//             return yCenter[d.gekozenTiktok];
+//         }))
+//         .force('collision', d3.forceCollide().radius(function(d) {
+//             return 25;
+//         }))
+//         .on('tick', ticked)
+//     ;
 
-    function ticked() {
-        d3.select('.bubbles')
-            .selectAll('circle')
-            .data(newData)
-            .join('circle')
-            .attr("r", 20)
-            .attr("fill", d => 
-                colorPicker(d)
-            )
-            .attr('cx', function(d) {
-                return d.x;
-            })
-            .attr('cy', function(d) {
-                return d.y;
-            });
-    };
-};
-function updateDeelgedrag(data, buttonPressed, whichBtn ) {
-    // Koppigheid
-    let newData;
-    if (buttonPressed) {
-        if (whichBtn == 1) {
-            newData = data.filter(d => d.deelGedrag == "Altijd");
-        } else if (whichBtn == 2) {
-            newData = data.filter(d => d.deelGedrag == "Vaak");
-        } else if (whichBtn == 3) {
-            newData = data.filter(d => d.deelGedrag == "Soms");
-        } else if (whichBtn == 4) {
-            newData = data.filter(d => d.deelGedrag == "Nooit");
-        }
-    } else {
-        newData = data;
-    };
+//     function ticked() {
+//         d3.select('.bubbles')
+//             .selectAll('circle')
+//             .data(newData)
+//             .join('circle')
+//             .attr("r", 20)
+//             .attr("fill", d => 
+//                 colorPicker(d)
+//             )
+//             .attr('cx', function(d) {
+//                 return d.x;
+//             })
+//             .attr('cy', function(d) {
+//                 return d.y;
+//             });
+//     };
+// };
+// function updateDeelgedrag(data, buttonPressed, whichBtn ) {
+//     // Koppigheid
+//     let newData;
+//     if (buttonPressed) {
+//         if (whichBtn == 1) {
+//             newData = data.filter(d => d.deelGedrag == "Altijd");
+//         } else if (whichBtn == 2) {
+//             newData = data.filter(d => d.deelGedrag == "Vaak");
+//         } else if (whichBtn == 3) {
+//             newData = data.filter(d => d.deelGedrag == "Soms");
+//         } else if (whichBtn == 4) {
+//             newData = data.filter(d => d.deelGedrag == "Nooit");
+//         }
+//     } else {
+//         newData = data;
+//     };
 
-    d3.forceSimulation(data)
-        .force('charge', d3.forceManyBody().strength(20))
-        .force('x', d3.forceX().x(function(d) {
-            return xCenter[d.gekozenTiktok];
-        }))
-        .force('y', d3.forceY().y(function(d) {
-            return yCenter[d.gekozenTiktok];
-        }))
-        .force('collision', d3.forceCollide().radius(function(d) {
-            return 25;
-        }))
-        .on('tick', ticked)
-    ;
+//     d3.forceSimulation(data)
+//         .force('charge', d3.forceManyBody().strength(20))
+//         .force('x', d3.forceX().x(function(d) {
+//             return xCenter[d.gekozenTiktok];
+//         }))
+//         .force('y', d3.forceY().y(function(d) {
+//             return yCenter[d.gekozenTiktok];
+//         }))
+//         .force('collision', d3.forceCollide().radius(function(d) {
+//             return 25;
+//         }))
+//         .on('tick', ticked)
+//     ;
 
-    function ticked() {
-        d3.select('.bubbles')
-            .selectAll('circle')
-            .data(newData)
-            .join('circle')
-            .attr("r", 20)
-            .attr("fill", d => 
-                colorPicker(d)
-            )
-            .attr('cx', function(d) {
-                return d.x;
-            })
-            .attr('cy', function(d) {
-                return d.y;
-            });
-    };
-};
+//     function ticked() {
+//         d3.select('.bubbles')
+//             .selectAll('circle')
+//             .data(newData)
+//             .join('circle')
+//             .attr("r", 20)
+//             .attr("fill", d => 
+//                 colorPicker(d)
+//             )
+//             .attr('cx', function(d) {
+//                 return d.x;
+//             })
+//             .attr('cy', function(d) {
+//                 return d.y;
+//             });
+//     };
+// };
 
 updateKoppigheid(data);
-updateDeelgedrag(data);
-updateSchermtijd(data);
+// updateDeelgedrag(data);
+// updateSchermtijd(data);
 
 d3.selectAll("#koppigheid button")
 .on("click", e => {
     updateKoppigheid(data, true, e.target.value);
+    e.target.classList.toggle("active");
 });
 
-d3.selectAll("#deelgedrag button")
-.on("click", e => {
-    updateDeelgedrag(data, true, e.target.value);
-});
+// d3.selectAll("#deelgedrag button")
+// .on("click", e => {
+//     updateDeelgedrag(data, true, e.target.value);
+// });
 
-d3.selectAll("#schermtijd button")
-.on("click", e => {
-    updateSchermtijd(data, true, e.target.value);
-});
+// d3.selectAll("#schermtijd button")
+// .on("click", e => {
+//     updateSchermtijd(data, true, e.target.value);
+// });
 // Bron: https://codepen.io/pen
 
 // Functions
@@ -544,63 +552,3 @@ function closeFilter() {
     filter.classList.remove("show");
 }
 gereedBtn.addEventListener("click", closeFilter);
-
-// Koppigheid buttons
-let prevButton = null;
-
-const koppigheid = document.querySelector("#koppigheid");
-
-koppigheid.addEventListener('click', (e) => {
-    const isButton = e.target.nodeName === 'BUTTON'; 
-    if (!isButton) {
-        return;
-    }
-    
-    e.target.classList.add('active'); // Add .active CSS Class
-
-    if(prevButton !== null) {
-        prevButton.classList.remove('active');  // Remove .active CSS Class
-    }
-    
-    prevButton = e.target;
-});
-
-// Deelgedrag buttons
-let prevBtn = null;
-
-const deelgedrag = document.querySelector("#deelgedrag");
-
-deelgedrag.addEventListener('click', (e) => {
-    const isButton = e.target.nodeName === 'BUTTON'; 
-    if (!isButton) {
-        return;
-    }
-    
-    e.target.classList.add('active'); // Add .active CSS Class
-
-    if(prevBtn !== null) {
-        prevBtn.classList.remove('active');  // Remove .active CSS Class
-    }
-    
-    prevBtn = e.target;
-});
-
-// Deelgedrag buttons
-let preBtn = null;
-
-const schermtijd = document.querySelector("#schermtijd");
-
-schermtijd.addEventListener('click', (e) => {
-    const isButton = e.target.nodeName === 'BUTTON'; 
-    if (!isButton) {
-        return;
-    }
-    
-    e.target.classList.add('active'); // Add .active CSS Class
-
-    if(preBtn !== null) {
-        preBtn.classList.remove('active');  // Remove .active CSS Class
-    }
-    
-    preBtn = e.target;
-});
